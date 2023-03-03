607ee204d03abe54cdf5552144aa725169acce6b
ifedayoprince@gmail.com <ifedayoprince@gmail.com>
2023-03-03 07:14:22 +0100

    New

--- core/pino-protocol.js
+++ core/pino-protocol.js
@@ -4,9 +4,7 @@ export class PinoResolver {
 	}
 	async resolve(app, origin, cb) {
 		let data = await (app.fetch(`pine.protocol.${origin}`));
-		console.log(data.data.protocol)
 		app.params = data.data.protocol.params;
-		alert(origin)
 		cb();
 	}
 }
